3efb71e32e14ad4a5229020bcfd24d71685378e1
Vitaly <126074186+VItaly0117@users.noreply.github.com>
2025-04-20 22:28:43 +0300

    Create homework_1.py

---
+++ homework_1.py
@@ -0,0 +1,18 @@
+a = int(input("Перше число:  "))
+b = int(input("Друге число:  "))
+c = int(input("Третє число:  "))
+
+sum = a + b + c
+dob = a * b * c
+
+print("Сума:  ", sum)
+print("Добуток:  ", dob)
+
+a = int(input("Зарплата : "))
+b = int(input("Сума : "))
+c = int(input("Заборгованість : "))
+
+
+zal = a - (b + c)
+
+print("Залишок після всіх виплат:", zal)
